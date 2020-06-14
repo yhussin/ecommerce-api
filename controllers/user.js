b5fb2ac56fc7b471842aacd5eaaa2b1ec50d99ba
yhussin <yhussin@gmail.com>
2020-06-14 10:37:01 -0700

    User controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,37 @@
-const User = require ('../models/user');
 
-exports.signup = (req, res) => {
-    console.log("req.body: ", req.body)
-    const user = new User(req.body);
-    user.save((err, user) => {
-        if(err) {
-            return res.status(400).json({
-                error
-            })
-        }
-        res.json({
-            user
-        })
-    })
+
+const db = require('../models')
+
+const show = (req, res) => {
+  db.User.findById(req.params.id, (err, foundUser) => {
+      if (err) return res.json({
+          message: err
+      })
+      res.status(200).json({
+          user: foundUser
+      })
+  })
 }
+
+module.exports = {
+    show 
+}
+
+
+// const User = require ('../models/user');
+
+// exports.signup = (req, res) => {
+//     console.log("req.body: ", req.body)
+//     const user = new User(req.body);
+//     user.save((err, user) => {
+//         if(err) {
+//             return res.status(400).json({
+//                 error
+//             })
+//         }
+//         res.json({
+//             user
+//         })
+//     })
+// }
+
