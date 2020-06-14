352377394c0d8469b54d57adeb90082e7343f205
yhussin <yhussin@gmail.com>
2020-06-14 11:44:45 -0700

    Adds routes and controllers index

--- models/user.js
+++ models/user.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose')
-const crypto = require('crypto')
-const uuidv1 = require('uuid/v1')
+// const crypto = require('crypto')
+// const uuidv1 = require('uuid/v1')
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         trim: true,
-        required: true,
+        required: [true, 'Name is required'],
         maxlength: 32
     },
     email: {
         type: String,
         trim: true,
-        required: true,
+        required: [true, 'email is required'],
         maxlength: 32
     },
     hash_password: {
         type: String,
-        required: true,
+        required: [true, 'password is required'],
     },
     salt: {
         type: String,
@@ -34,27 +34,30 @@ const userSchema = new mongoose.Schema({
     { timestamps: true }
 );
 
-userSchema.virtual('password')
-.set(function(password) {
-    this._password = password
-    this.salt = uuidv1()
-    this.hash_password = this.encryptPassword(password)
-})
-.get(function() {
-    return this._password
-})
+const User = mongoose.model('User', userSchema)
+module.exports = User
 
-userSchema.methods = {
-    encryptPassword: function(password) {
-        if(!password) return '';
-        try {
-            return crypto.createHmac('sha1', this.salt)
-               .update(password)
-               .digest('hex')
-        } catch (err) {
-            return ''
-        }
-    }
-}
+// userSchema.virtual('password')
+// .set(function(password) {
+//     this._password = password
+//     this.salt = uuidv1()
+//     this.hash_password = this.encryptPassword(password)
+// })
+// .get(function() {
+//     return this._password
+// })
 
-module.exports = mongoose.model('User', userSchema)
+// userSchema.methods = {
+//     encryptPassword: function(password) {
+//         if(!password) return '';
+//         try {
+//             return crypto.createHmac('sha1', this.salt)
+//                .update(password)
+//                .digest('hex')
+//         } catch (err) {
+//             return ''
+//         }
+//     }
+// }
+
+// module.exports = mongoose.model('User', userSchema)
