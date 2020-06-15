60052313bb879f9073224a4304a481923262c7f3
yhussin <yhussin@gmail.com>
2020-06-15 11:43:11 -0700

    deploy2

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 5000
 
-app.use(cors());
+const corsOptions = {
+    origin: [
+        `http://localhost:3000`, 
+        `https://wall-art.herokuapp.com`],
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 //app.use(morgan('dev'))
 app.use(bodyParser.json())
